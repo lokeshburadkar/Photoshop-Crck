d0f34cc34eaee682263fd35971664de60f8b8e39
Chandrashekhar Buradkar <buradkar.lokesh@gmail.com>
2024-04-10 18:05:57 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -209,6 +209,10 @@ def eda():
     else:
         return "No data available for EDA."
     
+@app.route('/')
+def home():
+    return render_template('main.html')
+    
 
 if __name__ == '__main__':
     app.run(debug=True,port=8000)
